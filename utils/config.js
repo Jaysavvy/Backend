8a988bb255db5e47fddc44b1887c34c17f13bc9e
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2023-05-22 14:19:36 -0400

    Made some changes to config file

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 
 const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGO_DB;
+const MONGODB_URI = process.env.NODE_ENV === 'test' 
+? process.env.TEST_MONGODB_URI
+: process.env.MONGO_DB;
+
+
 
 module.exports = {
   MONGODB_URI,
