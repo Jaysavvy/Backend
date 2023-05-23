c21e7fb767dab672644feea6632e8890bfadf2ea
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2023-05-23 11:42:37 -0400

    added app.js file and moved indxex

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
-const app = require('../app')
+const app = require('../index')
 
 const api = supertest(app)
 
 test('notes are returned as json', async () => {
   await api
-    .get('/api/notes')
+    .get('/api/')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
